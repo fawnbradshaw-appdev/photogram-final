9b51f48d0bf630e2c1a2fe38b9dba881c10bc91a
fawnbradshaw <fbradsha@chicagobooth.edu>
2023-03-10 17:20:51 +0000

    Photogram: First Draft

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  #Direct Associations
+  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+
+  has_many(:own_photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+
+  #Indirect Associations
+  has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  has_many(:followers, { :through => :received_follow_requests, :source => :sender })
+  has_many(:liked_photos, { :through => :likes, :source => :photo })
+  has_many(:feed, { :through => :following, :source => :own_photos })
+  has_many(:activity, { :through => :following, :source => :liked_photos })
+
+  #Validations
+  validates(:username, { :presence => true })
+  validates(:username, { :uniqueness => true })
+
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+end
