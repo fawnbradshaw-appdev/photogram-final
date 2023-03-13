1c007853ae33e4b76630161602a8a91476b63f59
fawnbradshaw <fbradsha@chicagobooth.edu>
2023-03-13 22:45:44 +0000

    Photogram Final: fixed cache counter errors

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -12,7 +12,7 @@
 class FollowRequest < ApplicationRecord
   #Direct Associations
   belongs_to(:sender, { :required => true, :class_name => "User", :foreign_key => "sender_id"})
-  belongs_to(:recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id", :counter_cache => :received_follow_requests_count })
+  belongs_to(:recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id"})
 
   #Validations
   validates(:sender_id, { :presence => true })
