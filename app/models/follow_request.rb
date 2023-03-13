ac9d58f31928a8242304125da0363d23b2484729
fawnbradshaw <fbradsha@chicagobooth.edu>
2023-03-13 22:41:57 +0000

    Photogram Final: After talking with Osha

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -11,7 +11,7 @@
 #
 class FollowRequest < ApplicationRecord
   #Direct Associations
-  belongs_to(:sender, { :required => true, :class_name => "User", :foreign_key => "sender_id", :counter_cache => :sent_follow_requests_count })
+  belongs_to(:sender, { :required => true, :class_name => "User", :foreign_key => "sender_id"})
   belongs_to(:recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id", :counter_cache => :received_follow_requests_count })
 
   #Validations
